8864baea67ae83f8d1df9235171acd9c27d3de84
igorlakic2 <igor.lakic2@gmail.com>
2020-05-11 15:26:03 +0200

    commit

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ const typing = () => {
 }
 
 window.addEventListener('load', typing);
+
+/*
+let sections = document.querySelectorAll(".sctn");
+let navLinks = document.querySelectorAll(".navLink");
+
+window.addEventListener("scroll", event => {
+    let fromTop = window.scrollY;
+
+    for(let i=0; i<navLinks.length; i++){
+
+    //}
+    //navLinks.forEach(link => {      
+        let s = document.querySelector(navLinks[i].hash);
+        console.log(navLinks[i]);
+
+        if (s.offsetTop <= fromTop && s.offsetTop + s.offsetHeight > fromTop) {
+            link.classList.add("active");
+          } else {
+            link.classList.remove("active");
+          }
+
+    }
+    //)
+})*/

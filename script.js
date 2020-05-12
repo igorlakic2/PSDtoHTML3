03b96ca08b0419e8d25f207899b42267c38f2a87
igorlakic2 <igor.lakic2@gmail.com>
2020-05-12 15:15:09 +0200

    commit

--- script.js
+++ script.js
@@ -11,26 +11,60 @@ const typing = () => {
 
 window.addEventListener('load', typing);
 
+let featuresOffsetTop = document.querySelector("#features");
+console.log(featuresOffsetTop.offsetTop);
+
 /*
-let sections = document.querySelectorAll(".sctn");
-let navLinks = document.querySelectorAll(".navLink");
+let sections = document.getElementsByClassName("sctn");
+let navLinks = document.getElementsByClassName("navLink");
 
 window.addEventListener("scroll", event => {
     let fromTop = window.scrollY;
 
-    for(let i=0; i<navLinks.length; i++){
+    for(let i=0; i<navLinks.length; i++) {
 
     //}
-    //navLinks.forEach(link => {      
-        let s = document.querySelector(navLinks[i].hash);
-        console.log(navLinks[i]);
 
-        if (s.offsetTop <= fromTop && s.offsetTop + s.offsetHeight > fromTop) {
+    //navLinks.forEach(link => {      
+        //let section = document.querySelector(link.hash);
+        let section = document.querySelector(navLinks[i].hash);
+
+        if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
             link.classList.add("active");
           } else {
             link.classList.remove("active");
           }
 
     }
-    //)
+    //);
 })*/
+
+let sections = document.querySelectorAll(".sctn");
+let navLinks = document.querySelectorAll(".navLink");
+
+const activeNavigation = () => {   
+    for(let i=0; i<sections.length; i++) {
+        navLinks[i].classList.remove("active");
+        //navLinks[i].style.color = 'black';
+
+        if(window.scrollY > (sections[i].offsetTop -5) && window.scrollY < (sections[i].offsetTop + sections[i].offsetHeight)){
+            console.log(navLinks[i].textContent);
+            navLinks[i].classList.add("active");
+            //navLinks[i].style.color = '#f2b31a';
+        }
+    }
+}
+const fixedNavigation = () => {
+    if(window.scrollY > 50){
+        document.querySelector('.navigation').classList.add('fixnav');
+        //document.querySelector('nav').style.width = '100%';        
+    }else{        
+        document.querySelector('.navigation').classList.remove('fixnav');
+        //document.querySelector('nav').style.width = '85%';
+    }
+}
+
+window.addEventListener('scroll', () => {
+    fixedNavigation();
+    activeNavigation();
+});
